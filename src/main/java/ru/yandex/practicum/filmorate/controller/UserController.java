c69d9f02f8410e1bbe1eb7e9d4157757b4210a66
Roman Shevlyako <roman.shevlyakov99@yandex.ru>
2024-11-13 19:25:24 +0300

    Добавление модлей user,film и контроллеров usercontroller,filmcontroller

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,86 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ElementNotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+@Validated
+public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+    private final Set<String> emailSet = new HashSet<>();
+    private Long userId = 1l;
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User createUser(@Valid @RequestBody User user) {
+        validUser(user);
+        user.setId(userId);
+        userId++;
+        users.put(user.getId(), user);
+        log.debug("Пользователь успешно создан");
+        return user;
+    }
+
+    public User update(@RequestBody User newUser) {
+        validUser(newUser);
+
+        if (newUser.getId() == null) {
+            throw new ValidationException("Id должен быть указан");
+        }
+
+        if (users.containsKey(newUser.getId())) {
+
+
+            User oldUser = users.get(newUser.getId());
+
+            if (!oldUser.getEmail().equals(newUser.getEmail()) && emailSet.contains(newUser.getEmail())) {
+                throw new ValidationException("Этот имейл уже используется");
+            }
+            if (newUser.getEmail() != null) {
+                oldUser.setEmail(newUser.getEmail());
+            }
+            if (newUser.getLogin() != null) {
+                oldUser.setLogin(newUser.getLogin());
+            }
+            if (newUser.getName() != null) {
+                oldUser.setName(newUser.getName());
+            }
+            if (newUser.getBirthday() != null) {
+                oldUser.setBirthday(newUser.getBirthday());
+            }
+            log.debug("Пользователь успешно обновлен");
+            return oldUser;
+        }
+        throw new ElementNotFoundException("id = " + newUser.getId() + " не найден");
+    }
+
+    private void validUser(User user) {
+        if (user.getEmail() != null && !user.getEmail().contains("@")) {
+            throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @");
+        }
+        if (user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
+            throw new ValidationException("логин не может быть пустым и содержать пробелы");
+        }
+        if (user.getName() == null && user.getLogin() != null) {
+            user.setName(user.getLogin());
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("дата рождения не может быть в будущем");
+        }
+    }
+
+}
