5683ae11ac2d8b18ae20c7c67b908bbfa2e0bfd1
Roman Shevlyako <roman.shevlyakov99@yandex.ru>
2024-12-31 18:59:18 +0300

    Исправления ошибки

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Data;
 
@@ -14,7 +13,6 @@ public class Film {
     public Long id;
     public String name;
     public String description;
-    @JsonIgnore
     public Integer rate; //Колличество лайков
     public LocalDate releaseDate;
     public @JsonFormat(shape = JsonFormat.Shape.NUMBER_INT)
